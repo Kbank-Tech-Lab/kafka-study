33a46d5fe092b634db4352a703ab0e962476beb9
WIN10HX64 <crm05055@snu.ac.kr>
2024-12-08 21:18:08 +0900

    feat(core-banking): error code
    - Updated an error code file

--- spring-core-banking/src/main/java/org/coreBanking/exception/ErrorCode.java
+++ spring-core-banking/src/main/java/org/coreBanking/exception/ErrorCode.java
@@ -8,12 +8,14 @@ public enum ErrorCode {
 
     // 계좌 관련 에러
     ACCOUNT_NOT_FOUND("COR0001", "Account not found"),
-    INSUFFICIENT_BALANCE("COR0002", "Insufficient account balance"),
-    TRANSFER_TO_SAME_ACCOUNT("COR0003", "Transfer to same account"),
-    TRANSFER_TO_OTHER_BANK("COR0004", "Transfer to other bank"),
 
     // 고객 관련 에러
-    CUSTOMER_NOT_FOUND("COR1001", "Customer not found");
+    CUSTOMER_NOT_FOUND("COR1001", "Customer not found"),
+
+    // 송금 관련 에러
+    INSUFFICIENT_BALANCE("COR2001", "Insufficient account balance"),
+    TRANSFER_TO_SAME_ACCOUNT("COR2002", "Transfer to same account"),
+    TRANSFER_TO_OTHER_BANK("COR2003", "Transfer to other bank");
 
 
     private final String code;
