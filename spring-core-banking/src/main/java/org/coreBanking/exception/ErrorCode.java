f9e91063d63b98640091ae425f3776fc7df2f98e
WIN10HX64 <crm05055@snu.ac.kr>
2024-12-08 21:07:49 +0900

    feat(core-banking): customer and deposit account entity, repository, service
    - Implemented customer and deposit account entity, repository, service

--- spring-core-banking/src/main/java/org/coreBanking/exception/ErrorCode.java
+++ spring-core-banking/src/main/java/org/coreBanking/exception/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     ACCOUNT_NOT_FOUND("COR0001", "Account not found"),
     INSUFFICIENT_BALANCE("COR0002", "Insufficient account balance"),
     TRANSFER_TO_SAME_ACCOUNT("COR0003", "Transfer to same account"),
-    TRANSFER_TO_OTHER_BANK("COR0004", "Transfer to other bank");
+    TRANSFER_TO_OTHER_BANK("COR0004", "Transfer to other bank"),
+
+    // 고객 관련 에러
+    CUSTOMER_NOT_FOUND("COR1001", "Customer not found");
 
 
     private final String code;
