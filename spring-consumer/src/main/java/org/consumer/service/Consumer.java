1f8591030c99c9e5eefd53834355601ee921c1ce
Cho-SangHyun <98shcho@naver.com>
2024-12-12 01:01:32 +0900

    feat(consumer) : 단일 서버에서의 글로벌락 구현
    - 메모리 상에 락 객체들을 두고 사용하는 형태로 구현
    - 해당 구현은 단일 서버에서만 유효하나, 추후 분산락 도입 예정에 따라 미리 간단히 구현해본 것임

--- spring-consumer/src/main/java/org/consumer/service/Consumer.java
+++ spring-consumer/src/main/java/org/consumer/service/Consumer.java
@@ -7,9 +7,12 @@ import org.consumer.dto.TransferStatusDto;
 import org.consumer.dto.MessageDto;
 import org.consumer.repository.DelayedTransferRepository;
 import org.consumer.util.ExternalApiClient;
+import org.consumer.util.LockManager;
 import org.consumer.util.TransferStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 @RequiredArgsConstructor
 @Component
@@ -18,6 +21,7 @@ public class Consumer {
     private final static String TOPIC = "delay-transfer-topic";
     private final static String GROUP_ID = "group1";
 
+    private final LockManager lockManager;
     private final ExternalApiClient externalApiClient;
     private final DelayedTransferRepository delayedTransferRepository;
 
@@ -27,6 +31,9 @@ public class Consumer {
         final MessageDto messageDto = record.value();
         log.info("Consumed message`s key: {}", userId);
         log.info("Consumed message`s value: {}", messageDto.toString());
+
+        ReentrantLock userLock = lockManager.getUserLock(userId);
+        userLock.lock();
 
         delayedTransferRepository.findById(messageDto.getId()).ifPresent(
                 transferStatusDto -> {
@@ -45,6 +52,8 @@ public class Consumer {
                 }
             }
         );
+
+        userLock.unlock();
     }
 
     private void saveTransferStatus(TransferStatusDto transferStatusDto,
