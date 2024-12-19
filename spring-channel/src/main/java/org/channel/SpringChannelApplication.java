d89021c162e2a1227cf7deca15caef4a0920148b
hunjin-choi <71176033+hunjin-choi@users.noreply.github.com>
2024-12-19 19:04:05 +0900

    불필요한 주석 제거

--- spring-channel/src/main/java/org/channel/SpringChannelApplication.java
+++ spring-channel/src/main/java/org/channel/SpringChannelApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-//@EntityScan(basePackages = {"org.channel.dto"} )
-//@EnableJpaRepositories(basePackages = {"org.channel.repository"})
 @SpringBootApplication
 public class SpringChannelApplication {
     public static void main(String[] args) {
